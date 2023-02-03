eeb36df82e69270a4e5adb6f856edd7397cb82b6
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-02-03 08:51:21 +0700

    add loginActivity

--- app/src/main/java/com/rizalfadiaalfikri/signlanguagerecognition/SplashScreenActivity.java
+++ app/src/main/java/com/rizalfadiaalfikri/signlanguagerecognition/SplashScreenActivity.java
@@ -2,7 +2,11 @@ package com.rizalfadiaalfikri.signlanguagerecognition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.rizalfadiaalfikri.signlanguagerecognition.loginRegister.LoginActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
+
     }
 }
---
+++ app/src/main/java/com/rizalfadiaalfikri/signlanguagerecognition/loginRegister/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.rizalfadiaalfikri.signlanguagerecognition.loginRegister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rizalfadiaalfikri.signlanguagerecognition.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
