67681749718a2cfb6f0da1b6573787388f15667c
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-03-11 11:26:30 +0700

    adding dependencies image picker

--- app/src/main/java/com/rizalfadiaalfikri/signlanguagerecognition/fragment/HomeFragment.java
+++ app/src/main/java/com/rizalfadiaalfikri/signlanguagerecognition/fragment/HomeFragment.java
@@ -1,22 +1,45 @@
 package com.rizalfadiaalfikri.signlanguagerecognition.fragment;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.rizalfadiaalfikri.signlanguagerecognition.R;
 
+import java.util.ArrayList;
+
 
 public class HomeFragment extends Fragment {
+
+    private CardView cardView;
+
+    String path;
+    Uri uri;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        cardView = view.findViewById(R.id.cardView);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Toast Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
